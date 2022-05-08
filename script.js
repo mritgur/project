52f79dbb0f71574adabc8856d526a458c23f939a
All_Lee <olegli@MacBook-Air-Oleg.local>
2022-05-08 17:26:24 +0700

    lesson19

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert('hello!');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors:{},
+    genres:[],
+    privat:false
+};
+
+const lasFilm1 = prompt('Один из последних просмотренных фильмов', '');
+const cost1 = prompt('На сколько оцените его','');
+const lasFilm2 = prompt('Один из последних просмотренных фильмов', '');
+const cost2 = prompt('На сколько оцените его','');
+
+personalMovieDB.movie[lasFilm1] = cost1;
+personalMovieDB.movie[lasFilm2] = cost2;
+
+console.log(personalMovieDB);
