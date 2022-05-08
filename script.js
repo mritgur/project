ec96ca3a89c90baa24e141170efb6d2d7f171866
All_Lee <olegli@MacBook-Air-Oleg.local>
2022-05-08 17:57:03 +0700

    les21

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movie: {},
-    actors:{},
-    genres:[],
-    privat:false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movie: {},
+//     actors:{},
+//     genres:[],
+//     privat:false
+// };
 
-const lasFilm1 = prompt('Один из последних просмотренных фильмов', '');
-const cost1 = prompt('На сколько оцените его','');
-const lasFilm2 = prompt('Один из последних просмотренных фильмов', '');
-const cost2 = prompt('На сколько оцените его','');
+// const lasFilm1 = prompt('Один из последних просмотренных фильмов', '');
+// const cost1 = prompt('На сколько оцените его','');
+// const lasFilm2 = prompt('Один из последних просмотренных фильмов', '');
+// const cost2 = prompt('На сколько оцените его','');
 
-personalMovieDB.movie[lasFilm1] = cost1;
-personalMovieDB.movie[lasFilm2] = cost2;
+// personalMovieDB.movie[lasFilm1] = cost1;
+// personalMovieDB.movie[lasFilm2] = cost2;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// if(4==4) {
+//     console.log('ok');
+// }
+const num = 50;
+// (num === 50 ) ? console.log('ok') : console.log('error');
+
+switch (num) {
+    case 49:
+        console.log('no');
+        break;
+    case 100:  
+    console.log('no');
+    break;
+    case 50:  
+    console.log('yes');
+    break;
+    default:
+        console.log('booooo');
+        break;
+}
